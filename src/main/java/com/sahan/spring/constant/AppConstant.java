d4dfee718e517263ee62bde9335a0b6acabd3be9
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-25 15:58:21 +0530

    Add files via upload

---
+++ src/main/java/com/sahan/spring/constant/AppConstant.java
@@ -0,0 +1,17 @@
+package com.sahan.spring.constant;
+
+public class AppConstant {
+    /**
+     * Customer and supplier CRUD
+     */
+    public static final String UPDATE_SUCCESSFUL = "Updated successfully";
+    public static final String SAVE_SUCCESSFUL = "Saved successfully";
+    public static final String DELETE_SUCCESSFUL = "Deleted successfully";
+
+    public static final String ADMIN_USER = "Admin";
+    public static final String ADMIN_PASSWORD = "admin";
+    public static final String ADMIN_RESPONSE = "ADMIN";
+    public static final String LOGIN_SUCCESSFUL = "Login Successfully.";
+
+
+}
---
+++ src/main/java/com/sahan/spring/constant/ExceptionMessageConstant.java
@@ -0,0 +1,18 @@
+package com.sahan.spring.constant;
+
+public class ExceptionMessageConstant {
+    /**
+     * Authentication
+     */
+    public static final String INVALID_PASSWORD = "Invalid password !";
+    public static final String INVALID_AUTH = "Invalid userName or password !";
+
+    /**
+     * Customer and supplier CRUD
+     */
+    public static final String CUSTOMER_ALREADY_SUBMITTED = "Customer already exist!";
+    public static final String SUPPLIER_ALREADY_SUBMITTED = "Supplier already exist!";
+    public static final String RECORD_NOT_FOUND = "Record not found for";
+    public static final String CUSTOMER_NOT_FOUND = "No such customer to update..!";
+    public static final String SUPPLIER_NOT_FOUND = "No such supplier to update..!";
+}
