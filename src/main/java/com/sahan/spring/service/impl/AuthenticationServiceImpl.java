143178d386e98b89b9f8054222e5fcaefff807f2
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-25 16:03:13 +0530

    exception handling

--- src/main/java/com/sahan/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/sahan/spring/service/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sahan.spring.service.impl;
 
 import com.sahan.spring.entity.UserDetail;
+import com.sahan.spring.exception.AuthRejectedException;
 import com.sahan.spring.repo.AuthenticationRepo;
 import com.sahan.spring.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
+
+import static com.sahan.spring.constant.AppConstant.*;
+import static com.sahan.spring.constant.ExceptionMessageConstant.INVALID_AUTH;
+import static com.sahan.spring.constant.ExceptionMessageConstant.INVALID_PASSWORD;
 
 @Service
 @Transactional
@@ -17,20 +22,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private AuthenticationRepo authenticationRepo;
 
     @Override
-    public String login(String userName, String password) {
-        if ((("Admin").equals(userName)) && (("admin").equals(password))) {
-            return "ADMIN";
+    public String login(String userName, String password) throws AuthRejectedException {
+        if (((ADMIN_USER).equals(userName)) && ((ADMIN_PASSWORD).equals(password))) {
+            return ADMIN_RESPONSE;
         }
         Optional<UserDetail> userDetail = authenticationRepo.findAllByUserName(userName);
         if (userDetail.isPresent()) {
             if ((userDetail.get().getUserName().equals(userName) && (userDetail.get().getPassword().equals(password)))) {
-                return "Login Successfully.";
+                return LOGIN_SUCCESSFUL;
             } else {
-                throw new RuntimeException("Invalid password !");
+                throw new AuthRejectedException(INVALID_PASSWORD);
             }
         } else {
-            throw new RuntimeException("Invalid userName or password !");
+            throw new AuthRejectedException(INVALID_AUTH);
         }
-
     }
 }
