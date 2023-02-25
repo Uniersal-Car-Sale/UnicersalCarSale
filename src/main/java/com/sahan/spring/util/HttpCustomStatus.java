a8e6a92108bf2cf43edb11dea09735d23cc8f09c
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-25 15:59:50 +0530

    Custom status code mapping

---
+++ src/main/java/com/sahan/spring/util/HttpCustomStatus.java
@@ -0,0 +1,7 @@
+package com.sahan.spring.util;
+
+public class HttpCustomStatus {
+    public static final int RECORD_NOT_FOUND = 600;
+    public static final int RECORD_ALREADY_SUBMIT = 601;
+    public static final int AUTHENTICATION_ERROR = 602;
+}
