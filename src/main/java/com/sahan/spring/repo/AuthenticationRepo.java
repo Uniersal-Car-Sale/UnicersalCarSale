82ba728a9df7fbdb141aa7e4bd6d69b4462c8e14
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-26 18:46:27 +0530

    Update AuthenticationRepo.java

--- src/main/java/com/sahan/spring/repo/AuthenticationRepo.java
+++ src/main/java/com/sahan/spring/repo/AuthenticationRepo.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AuthenticationRepo extends JpaRepository<UserDetail, Long> {
+public interface AuthenticationRepo extends JpaRepository<UserDetail, String> {
 
     Optional<UserDetail> findAllByUserName (String username);
 }
