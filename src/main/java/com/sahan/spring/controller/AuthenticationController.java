4dbbd51c6154e4d591af871107efec791cd73f8c
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-26 18:44:55 +0530

    register endpoint implementation

--- src/main/java/com/sahan/spring/controller/AuthenticationController.java
+++ src/main/java/com/sahan/spring/controller/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.sahan.spring.controller;
 
+import com.sahan.spring.dto.UserDetailDto;
 import com.sahan.spring.exception.AuthRejectedException;
 import com.sahan.spring.exception.BaseException;
+import com.sahan.spring.exception.RecordAlreadySubmittedException;
 import com.sahan.spring.service.AuthenticationService;
 import com.sahan.spring.util.HttpCustomStatus;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,22 @@ public class AuthenticationController {
     public ResponseEntity<?> signIn(@PathVariable String userName, @PathVariable String password) {
         try {
             return ResponseEntity.ok(authenticationService.login(userName, password));
+        } catch (AuthRejectedException e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpCustomStatus.AUTHENTICATION_ERROR).body(e.getMessage());
+        } catch (BaseException ex) {
+            log.error(ex.getMessage(), ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("register/credential")
+    public ResponseEntity<?> register(@RequestBody UserDetailDto userDetailDto) {
+        try {
+            return ResponseEntity.ok(authenticationService.registerCredentials(userDetailDto));
+        } catch (RecordAlreadySubmittedException e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpCustomStatus.RECORD_ALREADY_SUBMIT).body(e.getMessage());
         } catch (AuthRejectedException e) {
             log.error(e.getMessage(), e);
             return ResponseEntity.status(HttpCustomStatus.AUTHENTICATION_ERROR).body(e.getMessage());
