864f36262d75897a48848ef8e5c2415ad1c03d1b
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-02-26 18:47:15 +0530

    Update AuthenticationService.java

--- src/main/java/com/sahan/spring/service/AuthenticationService.java
+++ src/main/java/com/sahan/spring/service/AuthenticationService.java
@@ -1,5 +1,11 @@
 package com.sahan.spring.service;
 
+import com.sahan.spring.dto.UserDetailDto;
+
 public interface AuthenticationService {
     String login(String userName, String password);
+
+    String registerCredentials(UserDetailDto userDetailDto);
+
+
 }
